2969913b6635666b7d758a268db6569b98bf0731
andres-root <andres.root.coder@gmail.com>
2014-11-03 21:45:35 -0500

    update

--- neural-networks/gate.py
+++ neural-networks/gate.py
@@ -4,14 +4,19 @@
 def forward_multiply_gate(x, y):
     return x * y
 
+
+def get_derivative(new_out, out, h):
+    return (new_out - out) / h
+
+
 x = -2
 y = 3
-out = forward_multiply_gate(x, y)
 h = 0.001
+out = forward_multiply_gate(x, y)
 xph = x + h
 out1 = forward_multiply_gate(xph, y)
-x_derivative = (out1 - out) / h
+x_derivative = get_derivative(out1, out, h)
 
 ypy = y + h
 out2 = forward_multiply_gate(x, ypy)
-y_derivative = (out2 - out) / h
+y_derivative = get_derivative(out2, out, h)
