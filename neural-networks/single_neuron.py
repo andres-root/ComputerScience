981ed5fffa70778046a8bb3de9fef2b3427d009b
andres-root <andres.root.coder@gmail.com>
2014-11-04 23:09:54 -0500

    update neuron

--- neural-networks/single_neuron.py
+++ neural-networks/single_neuron.py
@@ -7,16 +7,20 @@ class Unit(object):
         self.value = value
         self.grad = grad
 
+    def get(self):
+        return self
+
 
 class MultiplyGate(object):
 
     def __init__(self, u0, u1):
         self.u0 = u0
         self.u1 = u1
-        self.utop = Unit(u0 * u1, 0.0)
+        self.utop = Unit(u0.value * u1.grad, 0.0)
 
     def forward(self):
         return self.utop
 
     def backward(self):
-        pass
+        self.u0.grad += self.u1.value * self.utop.grad
+        self.u1.grad += self.u0.value * self.utop.grad
