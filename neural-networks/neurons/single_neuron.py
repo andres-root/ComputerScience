e5d200fcfbda1f7bdf4a95fabd13a8d17105ee08
andres-root <andres.root.coder@gmail.com>
2014-11-04 23:15:34 -0500

    update neuron

--- neural-networks/neurons/single_neuron.py
+++ neural-networks/neurons/single_neuron.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 
 
-class Unit(object):
-
-    def __init__(self, value, grad):
-        self.value = value
-        self.grad = grad
-
-    def get(self):
-        return self
+from .components import Unit
 
 
 class MultiplyGate(object):
