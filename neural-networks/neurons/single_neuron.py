051662ecc5848d0eb47e7575c91d3e5c273066e6
andres-root <andres.root.coder@gmail.com>
2014-11-06 23:32:57 -0500

    update

--- neural-networks/neurons/single_neuron.py
+++ neural-networks/neurons/single_neuron.py
@@ -9,11 +9,28 @@ class MultiplyGate(object):
     def __init__(self, u0, u1):
         self.u0 = u0
         self.u1 = u1
-        self.utop = Unit(u0.value * u1.gradient, 0.0)
+        self.utop = 0
 
     def forward(self):
+        self.utop = Unit(self.u0.value * self.u1.gradient, 0.0)
         return self.utop
 
     def backward(self):
         self.u0.gradient += self.u1.value * self.utop.gradient
         self.u1.gradient += self.u0.value * self.utop.gradient
+
+
+class AddGate(object):
+
+    def __init__(self, u0, u1):
+        self.u0 = u0
+        self.u1 = u1
+        self.utop = 0
+
+    def forward(self):
+        self.utop = Unit(self.u0.value + self.u1.gradient, 0.0)
+        return self.utop
+
+    def backward(self):
+        self.u0.gradient += 1 * self.utop.gradient
+        self.u1.gradient += 1 * self.utop.gradient
