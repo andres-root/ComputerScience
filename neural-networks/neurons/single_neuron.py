15c2ae56ef812ecda86596f5d11290f4ba78106d
andres-root <andres.root.coder@gmail.com>
2014-11-06 23:49:04 -0500

    added sigmoid function

--- neural-networks/neurons/single_neuron.py
+++ neural-networks/neurons/single_neuron.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+# Python Modules
+import math
 
+# App Modules
 from .components import Unit
 
 
@@ -34,3 +37,33 @@ class AddGate(object):
     def backward(self):
         self.u0.gradient += 1 * self.utop.gradient
         self.u1.gradient += 1 * self.utop.gradient
+
+
+class SigmoidGate(object):
+
+    def __init__(self, u0):
+        self.u0 = u0
+        self.utop = 0
+        self.sigmoid = 0
+
+    def sigmoid(self, x):
+        """
+        Computes the sigmoid function wrt x.
+        http://en.wikipedia.org/wiki/Sigmoid_function
+        """
+        self.sigmoid = 1 / (1 + math.exp(-x))
+
+    def forward(self):
+        self.utop = Unit(self.sigmoid(self.u0.value), 0.0)
+
+    def backward(self):
+        sig = self.sigmoid(self.u0.value)
+        self.u0.gradient += (sig * (1 - sig)) * self.utop.gradient
+
+
+
+
+
+
+
+
