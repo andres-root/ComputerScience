0e8e0ca81a32f87e89679203ecf4783532ec518a
andres-root <andres.root.coder@gmail.com>
2014-11-20 23:00:26 -0500

    added sigmoid gate to module

--- machine-learning/gates.py
+++ machine-learning/gates.py
@@ -34,3 +34,26 @@ class MultiplyGate(object):
     def backward(self):
         self.u0.gradient += self.u1.value * self.utop.gradient
         self.u1.gradient += self.u0.value * self.utop.gradient
+
+
+class SigmoidGate(object):
+
+    def sigmoid(self, x):
+        """
+        Computes the sigmoid function wrt x.
+        Reference: http://en.wikipedia.org/wiki/Sigmoid_function
+        """
+        return (1 / (1 + math.exp(-x)))
+
+    def forward(self, u0):
+        self.u0 = u0
+        self.utop = Unit(self.sigmoid(self.u0.value), 0.0)
+        return self.utop
+
+    def backward(self):
+        """
+        Computes the local derivative with respect to its input,
+        then multiplies on the gradient from the unit above.
+        """
+        sig = self.sigmoid(self.u0.value)
+        self.u0.gradient += (sig * (1 - sig)) * self.utop.gradient
