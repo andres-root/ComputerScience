fe37b8f3d3b60a7cdb61f8dc56b1b55e5804de51
andres-root <andres.root.coder@gmail.com>
2014-11-20 23:15:32 -0500

    added forward function to the circuit

--- machine-learning/support_vector_machine.py
+++ machine-learning/support_vector_machine.py
@@ -11,3 +11,10 @@ class Circuit(object):
         self.mulg1 = MultiplyGate()
         self.addg0 = AddGate()
         self.addg1 = AddGate()
+
+    def forward(self, a, b, c, x, y):
+        self.ax = self.mulg0.forward(a, x)
+        self.by = self.mulg1.forward(b, y)
+        self.axpby = self.addg0.forward(self.ax, self.by)
+        self.axpbypc = self.addg1.forward(self.axpby, c)
+        return self.axpbypc
