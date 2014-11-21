b7c7492b54aa6b3b0af5b7fe0745df8940391ea8
andres-root <andres.root.coder@gmail.com>
2014-11-20 22:59:34 -0500

    added add gate to module

--- machine-learning/gates.py
+++ machine-learning/gates.py
@@ -21,3 +21,16 @@ class MultiplyGate(object):
     def backward(self):
         self.u0.gradient += self.u1.value * self.utop.gradient
         self.u1.gradient += self.u0.value * self.utop.gradient
+
+
+class AddGate(object):
+
+    def forward(self, u0, u1):
+        self.u0 = u0
+        self.u1 = u1
+        self.utop = Unit(self.u0.value + self.u1.value, 0.0)
+        return self.utop
+
+    def backward(self):
+        self.u0.gradient += 1 * self.utop.gradient
+        self.u1.gradient += 1 * self.utop.gradient
