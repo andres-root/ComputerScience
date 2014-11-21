5135c4f9ce6580e3d57fea5dc0e365c84a608266
andres-root <andres.root.coder@gmail.com>
2014-11-20 22:59:03 -0500

    added multiply gate to module

--- machine-learning/gates.py
+++ machine-learning/gates.py
@@ -8,3 +8,16 @@ class Unit(object):
     def __init__(self, value, gradient):
         self.value = value
         self.gradient = gradient
+
+
+class MultiplyGate(object):
+
+    def forward(self, u0, u1):
+        self.u0 = u0
+        self.u1 = u1
+        self.utop = Unit(self.u0.value * self.u1.value, 0.0)
+        return self.utop
+
+    def backward(self):
+        self.u0.gradient += self.u1.value * self.utop.gradient
+        self.u1.gradient += self.u0.value * self.utop.gradient
