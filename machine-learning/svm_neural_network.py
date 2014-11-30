69bfe0532df2c2e54aabc3a81543b7b181fcfad8
andres-root <andres.root.coder@gmail.com>
2014-11-29 20:57:24 -0500

    update

--- machine-learning/svm_neural_network.py
+++ machine-learning/svm_neural_network.py
@@ -4,6 +4,11 @@ import math
 from random import random
 
 a1 = random() - 0.5
-
+dataset = [{'data': (1.2, 0.7), 'label': 1},
+           {'data': (-0.3, -0.5), 'label': -1},
+           {'data': (3.0, 0.1), 'label': 1},
+           {'data': (-0.1, -1.0), 'label': -1},
+           {'data': (-1.0, 1.1), 'label': -1},
+           {'data': (-1.0, 1.1), 'label': 1}]
 for iteration in range(0, 400):
-	pass
+    i = math.floor(random() * len(dataset))
