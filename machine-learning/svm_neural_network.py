4a7efcf108b86895ea87d9b7a5b0aaa0d966b5ce
andres-root <andres.root.coder@gmail.com>
2014-11-29 20:55:30 -0500

    update

--- machine-learning/svm_neural_network.py
+++ machine-learning/svm_neural_network.py
@@ -3,31 +3,7 @@
 import math
 from random import random
 
-a = 1
-b = -2
-c = -1
-dataset = [{'data': (1.2, 0.7), 'label': 1},
-           {'data': (-0.3, -0.5), 'label': -1},
-           {'data': (3.0, 0.1), 'label': 1},
-           {'data': (-0.1, -1.0), 'label': -1},
-           {'data': (-1.0, 1.1), 'label': -1},
-           {'data': (-1.0, 1.1), 'label': 1}]
-for iteration in range(1, 401):
-    i = math.floor(random() * len(dataset))
-    x = dataset[i]['data'][0]
-    y = dataset[i]['data'][1]
-    label = dataset[i]['label']
+a1 = random() - 0.5
 
-    score = a*x + b*y + c
-    pull = 0.0
-    if label == 1 and score < 1:
-        pull = 1
-    if label == -1 and score > -1:
-        pull = -1
-
-    step_size = 0.01
-    a += step_size * (x * pull - a)
-    b += step_size * (y * pull - b)
-    c += step_size * (1 * pull)
-    if iteration % 25 == 0:
-        print('Iteration {0} {1}'.format(iteration, score))
+for iteration in range(0, 400):
+	pass
