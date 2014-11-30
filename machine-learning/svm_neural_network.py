89f6e2a58d7c9a8e8ec95de07a103f536746e569
andres-root <andres.root.coder@gmail.com>
2014-11-29 21:21:20 -0500

    update

--- machine-learning/svm_neural_network.py
+++ machine-learning/svm_neural_network.py
@@ -46,3 +46,78 @@ for iteration in range(0, 400):
         pull = 1
     if label == -1 and score > -1:
         pull = -1
+
+    # now compute backward pass to all parameters of the model
+    # backprop through the last "score" neuron
+    dscore = pull
+    da4 = n1 * dscore
+    dn1 = a4 * dscore
+    db4 = n2 * dscore
+    dn2 = b4 * dscore
+    dc4 = n3 * dscore
+    dn3 = c4 * dscore
+    dd4 = 1.0 * dscore
+
+    # backprop the ReLU non-linearities, in place
+    # i.e. just set gradients to zero if the neurons did not "fire"
+    dn3 = n3 === 0 ? 0 : dn3
+    dn2 = n2 === 0 ? 0 : dn2
+    dn1 = n1 === 0 ? 0 : dn1
+
+    # backprop to parameters of neuron 1
+    da1 = x * dn1
+    db1 = y * dn1
+    dc1 = 1.0 * dn1
+
+    # backprop to parameters of neuron 2
+    da2 = x * dn2
+    db2 = y * dn2
+    dc2 = 1.0 * dn2
+
+    # backprop to parameters of neuron 3
+    da3 = x * dn3
+    db3 = y * dn3
+    dc3 = 1.0 * dn3
+
+    # phew! End of backprop!
+    # note we could have also backpropped into x,y
+    # but we do not need these gradients. We only use the gradients
+    # on our parameters in the parameter update, and we discard x,y
+
+    # add the pulls from the regularization, tugging all multiplicative
+    # parameters (i.e. not the biases) downward, proportional to their value
+    da1 += -a1
+    da2 += -a2
+    da3 += -a3
+    db1 += -b1
+    db2 += -b2
+    db3 += -b3
+    da4 += -a4
+    db4 += -b4
+    dc4 += -c4
+
+    # finally, do the parameter update
+    step_size = 0.01
+    a1 += step_size * da1
+    b1 += step_size * db1
+    c1 += step_size * dc1
+    a2 += step_size * da2
+    b2 += step_size * db2
+    c2 += step_size * dc2
+    a3 += step_size * da3
+    b3 += step_size * db3
+    c3 += step_size * dc3
+    a4 += step_size * da4
+    b4 += step_size * db4
+    c4 += step_size * dc4
+    d4 += step_size * dd4
+
+
+
+
+
+
+
+
+
+
