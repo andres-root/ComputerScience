cac222fc6d2dce012d8d22cbd28de8a990fb89ee
andres-root <andres.root.coder@gmail.com>
2014-12-01 23:34:49 -0500

    initializing functions

--- machine-learning/2d_svm.py
+++ machine-learning/2d_svm.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+def cost(X, y, w):
+    pass
+
+
+if __name__ == '__main__':
+    X = [[1.2, 0.7], [-0.3, 0.5], [3, 2.5]]
+    y = [1, -1, 1]
+    w = [0.1, 0.2, 0.3]
+    alpha = 0.1
+    print(cost())
